77bb4c7dc1b3ff56a05a0cb23a19c44265a089b2
clementAvesque <clement2avesque@gmail.com>
2025-08-06 14:24:12 +0200

    correction ajout de joueur

--- main.js
+++ main.js
@@ -3,33 +3,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 const { createClient } = require('@supabase/supabase-js')
+
 const supabaseUrl = process.env.DATABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
-console.log("Hello, Node.js !");
 
-async function testConnection() {
-  const { error } = await supabase.from('*').select('*').limit(1)
-  if (!error) {
-    console.error("tu t'es bien connecté a supabase")
-  }
-}
+console.log("Hello, Node.js !")
+
 app.use(express.json())
 
 app.post('/creation_joueur', async (req, res) => {
-  const { discordId, userId } = req.body
+  const { discordId } = req.body
 
-  if (!discordId || !userId) {
-    return res.json({ error: 'Discord ID and User ID are required' })
-  }
-  else{
-    const { data, error } = await supabase
+  if (!discordId) {
+    return res.status(400).json({ error: "Discord ID est requis." })
+  } else {
+
+    await supabase
       .from('joueurs')
-      .insert([{ discord_id: discordId, user_id: userId }])
+      .insert([{ discord_id: discordId }])  // pas besoin de user_id ici
+
+    if (error) {
+      console.error("Erreur insertion :", error)
+      return res.json({ message: "Erreur lors de la création du joueur." })
+    }
+
+    return res.json({ message: "Joueur créé avec succès.", data })
   }
-
 })
-
 app.listen(port, () => {
   console.log(`🚀 Serveur API en écoute sur http://localhost:${port}`)
 })
