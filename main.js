3c37be4129823a979f1908821ea5eabe078ffdab
clementAvesque <clement2avesque@gmail.com>
2025-08-06 17:25:23 +0200

    ajout du port de render

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const { createClient } = require('@supabase/supabase-js')
 
 const supabaseUrl = process.env.DATABASE_URL
